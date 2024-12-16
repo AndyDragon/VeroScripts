5fbe20a05d2920badc67cadf5a95d4ebaeb9712e
Andrew Forget <andrew.forget@andydragon.com>
2024-12-16 16:19:24 -0500

    Added code to Windows app to remember maximized location

--- Vero Scripts Windows/Vero Scripts/MainWindow.xaml.cs
+++ Vero Scripts Windows/Vero Scripts/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MahApps.Metro.Controls;
+using System.ComponentModel;
+using System.Windows;
 
 namespace VeroScripts
 {
@@ -25,6 +27,40 @@ namespace VeroScripts
             if (this.DataContext is ScriptsViewModel viewModel)
             {
                 viewModel.MainWindow = this;
+            }
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                // Use the RestoreBounds as the current values will be 0, 0 and the size of the screen
+                Properties.Settings.Default.Top = RestoreBounds.Top;
+                Properties.Settings.Default.Left = RestoreBounds.Left;
+                Properties.Settings.Default.Height = RestoreBounds.Height;
+                Properties.Settings.Default.Width = RestoreBounds.Width;
+                Properties.Settings.Default.Maximized = true;
+            }
+            else
+            {
+                Properties.Settings.Default.Top = Top;
+                Properties.Settings.Default.Left = Left;
+                Properties.Settings.Default.Height = Height;
+                Properties.Settings.Default.Width = Width;
+                Properties.Settings.Default.Maximized = false;
+            }
+            Properties.Settings.Default.Save();
+        }
+        private void OnSourceInitialized(object sender, EventArgs e)
+        {
+            this.Top = Properties.Settings.Default.Top;
+            this.Left = Properties.Settings.Default.Left;
+            this.Height = Properties.Settings.Default.Height;
+            this.Width = Properties.Settings.Default.Width;
+            // Very quick and dirty - but it does the job
+            if (Properties.Settings.Default.Maximized)
+            {
+                WindowState = WindowState.Maximized;
             }
         }
     }
