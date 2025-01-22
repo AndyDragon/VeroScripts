0cc09fdb6f55456bb038b04eec2389971bdc9136
Andrew Forget <andrew.forget@andydragon.com>
2025-01-21 22:09:55 -0500

    Added screenshot prep script

---
+++ prepare-app-for-screenshot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+defaults write com.andydragon.Vero-Scripts "NSWindow Frame NSNavPanelAutosaveName" "0 0 1280 800 0 0 1280 800 "
+defaults write com.andydragon.Vero-Scripts "NSWindow Frame SwiftUI.ModifiedContent<SwiftUI.ModifiedContent<Vero_Scripts.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<Vero_Scripts.AppCommandModel>>>, SwiftUI._AppearanceActionModifier>-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
+defaults write com.andydragon.Vero-Scripts "NSWindow Frame SwiftUI.ModifiedContent<Vero_Scripts.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<Vero_Scripts.AppCommandModel>>>-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
+defaults write com.andydragon.Vero-Scripts "NSWindow Frame Vero_Scripts.ContentView-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
+defaults write com.andydragon.Vero-Scripts "NSWindow Frame Vero_Scripts__Script_Testing_.ContentView-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
+defaults write com.andydragon.Vero-Scripts "NSWindow Frame SwiftUI.ModifiedContent<Vero_Scripts.ContentView, SwiftUI._EnvironmentKeyWritingModifier<Swift.Optional<Vero_Scripts.AppCommandModel>>>-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
+defaults write com.andydragon.Vero-Scripts "NSWindow Frame SwiftUI.ModifiedContent<Vero_Scripts.ContentView, SwiftUI._AppearanceActionModifier>-1-AppWindow-1" "0 0 1280 800 0 0 1280 800 "
